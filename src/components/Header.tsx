2409c37a8029b8d95339cbff18693e22e20ded6e
gamadv <lealdev.py@gmail.com>
2022-11-07 20:18:26 -0300

    feat: Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import data from "../__mocks__/data.json";
+
+export function Header() {
+  return (
+    <StyledHeader>
+      {/* <img src="banner" /> */}
+      <section className="user-info">
+        <img src={`https://github.com/${data.github}.png`} />
+        <div>
+          <h2>{data.name}</h2>
+          <p>{data.description}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  );
+}
+
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+  .user-info {
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
+`;
