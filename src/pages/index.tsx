d3f678ac42e24b7e006cc518557b13c516442288
gamadv <lealdev.py@gmail.com>
2022-11-07 20:19:17 -0300

    feat: Insert new components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import data from "../__mocks__/data.json";
+import { CSSReset } from "../styles/global";
+import Menu from "../components/Menu";
+import { Header } from "../components/Header";
+import { Timeline } from "../components/Timeline";
 
-export default function Home() {
+function HomePage() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <CSSReset />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+        }}
+      >
+        <Menu />
+        <Header />
+        <Timeline playlists={data.playlists}>Conteúdo</Timeline>
+      </div>
+    </>
+  );
 }
+
+export default HomePage;
