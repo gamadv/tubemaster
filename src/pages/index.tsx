4511bad5cc916585401aece15bc03f450bc13c9e
gamadv <lealdev.py@gmail.com>
2022-11-11 12:21:13 -0300

    Change data on Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,33 @@
-import { useState } from "react";
-import data from "../__mocks__/data.json";
+import { useEffect, useState } from "react";
 import { CSSReset } from "../styles/global";
 import Menu from "../components/Menu";
 import { Header } from "../components/Header";
 import { Timeline } from "../components/Timeline";
+import { videoService } from "../services/videoServices";
 
 function HomePage() {
+  const service = videoService();
   const [valorDoFiltro, setValorDoFiltro] = useState("");
+  const [playlists, setPlaylists] = useState({});
+
+  useEffect(() => {
+    console.log("useEffect");
+    service.getAllVideos().then((dados) => {
+      console.log(dados.data);
+      // Forma imutavel
+      const novasPlaylists = {};
+      dados.data.forEach((video) => {
+        if (!novasPlaylists[video.playlist])
+          novasPlaylists[video.playlist] = [];
+        novasPlaylists[video.playlist] = [
+          video,
+          ...novasPlaylists[video.playlist],
+        ];
+      });
+
+      setPlaylists(novasPlaylists);
+    });
+  }, []);
 
   return (
     <>
@@ -18,9 +39,14 @@ function HomePage() {
           flex: 1,
         }}
       >
-        <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro}/>
+        <Menu
+          valorDoFiltro={valorDoFiltro}
+          setValorDoFiltro={setValorDoFiltro}
+        />
         <Header />
-        <Timeline  searchValue={valorDoFiltro} playlists={data.playlists}>Conteúdo</Timeline>
+        <Timeline searchValue={valorDoFiltro} playlists={playlists}>
+          Conteúdo
+        </Timeline>
       </div>
     </>
   );
